fd589b26e0f9d527ff5574ad603e4bc47d9c7f96
kaneez <k.fatima.dev@gmail.com>
2024-02-24 15:33:19 +0500

    initial commit

---
+++ test.js
@@ -0,0 +1,115 @@
+const input = document.getElementById("exampleInputEmail1");
+const replyText = document.querySelectorAll(".reply");
+const container = document.querySelector(".container");
+
+const commentDiv = (item, parentId = null) => {
+  let div = document.createElement("div");
+  console.log(item)
+  div.innerHTML = `
+      <p class="comment" id="comment-${item.id}">${item.comment}</p> 
+      <p class="text-black ms-2 reply" onclick="reply(${item.id})">Reply</p>
+      <div id="replyText-${item.id}"></div>`;
+  if (parentId === null) {
+    container.appendChild(div);
+  } else {
+    let replyTextDiv = document.getElementById(`replyText-${parentId}`);
+    if (replyTextDiv) {
+      replyTextDiv.appendChild(div);
+    }
+  }
+
+  if (item.replies && item.replies.length > 0) {
+    item.replies.forEach((reply) => {
+      commentDiv(reply, item.id);
+    });
+  }
+};
+const reply = (commentId) => {
+    console.log(commentId)
+ let replyInput = document.createElement("div");
+    replyInput.classList.add("mb-3", "d-flex");
+    replyInput.innerHTML = `
+        <input type="text" class="form-control" id="replyInput-${commentId}"  placeholder="Add a Reply">
+        <span class="ms-3 mt-2 text-primary" onclick="postReply(${commentId})">Reply</span>`;
+  
+    let replyTextDiv = document.getElementById(`replyText-${commentId}`);
+    if (replyTextDiv) {
+      replyTextDiv.appendChild(replyInput);
+    }
+    console.log(replyTextDiv)
+  };
+  
+  const postReply = (commentId) => {
+    const replyInput = document.getElementById(`replyInput-${commentId}`);
+    const storedData = JSON.parse(localStorage.getItem("comment")) || [];
+  
+    const parentComment = findParentComment(commentId, storedData);
+  
+   
+  
+    if (!parentComment.replies) {
+      parentComment.replies = [];
+    }
+    const replyComment = {
+ id: storedData.length,
+       comment: replyInput.value,
+       parentId: commentId,
+       replies: [],
+     };
+    parentComment.replies.push(replyComment);
+  
+    localStorage.setItem("comment", JSON.stringify(storedData));
+  
+    let div2 = document.createElement("div");
+    div2.innerHTML = `
+        <p class="comment post-reply" id="comment-${replyComment.id}">${replyComment.comment}</p> 
+        <p class="text-black ms-2 reply" onclick="reply(${replyComment.id})">Reply</p>
+        <div id="replyText-${replyComment.id}"></div>`;
+  
+    let replyTextDiv = document.getElementById(`replyText-${commentId}`);
+    if (replyTextDiv) {
+      replyTextDiv.appendChild(div2);
+    }
+  
+    replyInput.remove();
+  };
+   
+
+const findParentComment = (commentId, comments) => {
+  for (const comment of comments) {
+    if (comment.id === commentId) {
+      return comment;
+    }
+    if (comment.replies && comment.replies.length > 0) {
+      const parentComment = findParentComment(commentId, comment.replies);
+      if (parentComment) {
+        return parentComment;
+      }
+    }
+  }
+  return null;
+};
+
+const displaydata = () => {
+  getData = localStorage.getItem("comment");
+  if (getData) {
+    let parseData = JSON.parse(getData);
+    parseData.forEach((value) => {
+      commentDiv(value);
+    });
+  }
+};
+
+const post = () => {
+  const storedData = JSON.parse(localStorage.getItem("comment")) || [];
+  let comment = {
+    id: storedData.length,
+    comment: input.value,
+    replies: []
+  };
+  storedData.push(comment);
+  localStorage.setItem("comment", JSON.stringify(storedData));
+  commentDiv(comment);
+};
+
+displaydata();
